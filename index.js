a3a438e154c534ab2e5cd4c70a1ada43b0dfb4fc
Sheila <rugut.cheruto@student.moringaschool.com>
2022-08-22 12:30:52 +0300

    completed assignment

--- index.js
+++ index.js
@@ -11,6 +11,26 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+/*const titleCased = () => {
+  return tutorials.map((line)=>{
+//Call the split() method to get an array containing the words
+//lowercase the entire string to make sure that only the first letter of each word is uppercase.
+    const array=line.toLowerCase().split(" ");
+//Call the map() method to iterate over the array and capitalize the first character of each word.
+    const capitalizedWords=array.map((token)=>token.charAt(0).toUpperCase() +token.slice(1));
+   // Join the array of words into a string, using the join() method.
+    const result=capitalizedWords.join(" ");
+    return result;
+  });
+};*/
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((array)=>{
+    //Call the split() method to get an array containing the words
+    const word=array.split(" ");
+    //Call the map() method to iterate over the array and capitalize the first character of each word.
+    const capitalizedWords=word.map((token)=>token.charAt(0).toUpperCase() +token.slice(1));
+    // Join the array of words into a string, using the join() method.
+    const response=capitalizedWords.join(" ");
+    return response;
+  });
+};
